b381a212b7eae60dcc9df3c333bdff430be8d8b6
Caurs <peteris.caurs@accenture.com>
2020-11-06 14:37:03 +0200

    Style setup and routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import Particles from 'react-particles-js';
+import particlesConfig from './particlesjs-config.json';
+import './app.scss';
+
 const App = () => {
   return (
-    <>
+    <div>
+      <Particles
+        className="particles"
+        params={particlesConfig} />
       burtu juceklis
-    </>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import '@fortawesome/fontawesome-free/css/all.min.css'; 
+import 'bootstrap-css-only/css/bootstrap.min.css'; 
+import 'mdbreact/dist/css/mdb.css';
+import "./assets/scss/mdb-free.scss";
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
